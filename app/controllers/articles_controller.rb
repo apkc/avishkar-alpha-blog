abd8eb16e03c5bfdc285927e3ff26c828eec2187
apkc <avishkarpratapkc@gmail.com>
2016-12-10 15:29:00 +0545

    modified the shared partials and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was saved successfully sir!!!"
+      flash[:success] = "Article was saved successfully sir!!!"
       redirect_to article_path(@article) 
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully"
+      flash[:success] = "Article was updated successfully"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice]="Article was deleted successfully myaaan!"
+    flash[:danger]="Article was deleted successfully myaaan!"
     redirect_to articles_path
   end
 
